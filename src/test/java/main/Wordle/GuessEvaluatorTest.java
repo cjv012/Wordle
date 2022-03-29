44ff7721a4bfd4db5225b7c8776917a913e6e718
Connor Vucovich <cjv012@bucknell.edu>
2022-03-29 03:46:44 -0400

    "GuessEvaluatorTest Class Complete"

---
+++ src/test/java/main/Wordle/GuessEvaluatorTest.java
@@ -0,0 +1,54 @@
+package main.Wordle;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GuessEvaluatorTest {
+
+    /**
+     * Instance variable of the Guess Evaluator method
+     */
+    private GuessEvaluator guesser = new GuessEvaluator();
+
+    /**
+     * A simple test for the setSecretWord method
+     */
+    @Test
+    void setSecretWord() {
+        assertEquals(guesser.getSecrectWord(), null);
+        guesser.setSecretWord("hello");
+        assertEquals(guesser.getSecrectWord(), "hello");
+    }
+
+    /**
+     * A simple test for the AnalyzeGuess method
+     */
+    @Test
+    void analyzeGuess() {
+        guesser.setSecretWord("house");
+        String return_string = guesser.analyzeGuess("horse");
+        assertTrue(return_string.equals("**-**"));
+        return_string = guesser.analyzeGuess("shoal");
+        assertTrue(return_string.equals("+++--"));
+        return_string = guesser.analyzeGuess("muddy");
+        assertTrue(return_string.equals("-+---"));
+        return_string = guesser.analyzeGuess("house");
+        assertTrue(return_string.equals("*****"));
+        return_string = guesser.analyzeGuess("drain");
+        assertTrue(return_string.equals("-----"));
+
+
+    }
+
+    /**
+     * A simple test for the validGuess method
+     */
+    @Test
+    void validGuess() {
+        assertFalse(guesser.validGuess("10wor"));
+        assertTrue(guesser.validGuess("hello"));
+        assertFalse(guesser.validGuess("apples"));
+
+    }
+}
